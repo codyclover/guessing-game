6287f4f0c5cba240731e0dd81daf290b26eae3d9
Cody Clover <CodyClover@Codys-MacBook-Air.local>
2016-01-20 18:24:53 -0800

    Update for practice

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-
-var questionRight= 0;
-var questionWrong= 0;
-var userName = prompt ('Welcome, who is playing the game?');
-var questions = [' Hey there ' + ( userName ) + ',' + ' What\'s my favorite animal, Meerkat or Llama?', ' Next Question, ' + (userName) + ',' + ' What\'s my favorite musician? Pick between Frank Zappa or John Tesh. ', 'Next up ' + (userName) + ',' + ' pick my favorite vegetable, Avocado or Beets?', 'Another question ' + (userName) + ',' + ' What state was I born in? Oklahoma or Washington?'];
+var questionRight = 0;
+var questionWrong = 0;
+var userName = prompt('Welcome, who is playing the game?');
+var questions = [
+  ' Hey there ' + ( userName ) + ',' + ' What\'s my favorite animal, Meerkat or Llama?',
+  ' Next Question, ' + (userName) + ',' + ' What\'s my favorite musician? Pick between Frank Zappa or John Tesh. ',
+  'Next up ' + (userName) + ',' + ' pick my favorite vegetable, Avocado or Beets?',
+  'Another question ' + (userName) + ',' + ' What state was I born in? Oklahoma or Washington?'
+];
 var answers = ['meerkat', 'frank zappa', 'avocado', 'oklahoma']
-
-var firstQuestion= document.getElementById('El1');
-var correctAnswers= document.getElementById("El2");
-
-
+var firstQuestion = document.getElementById('el1');
+var correctAnswers = document.getElementById('el2');
 
 function game(questions, answers) {
   var ques = prompt(questions).toLowerCase();
@@ -26,26 +27,22 @@ for (var i = 0; i < questions.length; i += 1) {
   game(questions[i], answers[i]);
 }
 var userNumber = parseInt(prompt('Another question ' + (userName) + ',' + ' Guess my favorite number, between 1 and 10!'));
-    console.log(typeof userNumber);
+console.log(typeof userNumber);
 
 while (userNumber !== 5) {
   if (userNumber < 5) {
-    questionWrong++
     console.log('You guessed a little low,' + (userName));
     alert('You guessed a little low,' + (userName));
   } else if (userNumber > 5) {
-    questionWrong++
     console.log('You guessed too high, ' + (userName));
     alert('You guessed a little high,' + (userName));
-
   }
   userNumber = parseInt(prompt('Guess my favorite number!'));
 }
 
-  questionRight++
-  console.log('Congrats ' + (userName) + ',' + ' That\'s the one!');
-  alert('Congrats ' + (userName) + ',' + ' That\'s the one!');
-
+questionRight++;
+console.log('Congrats ' + (userName) + ',' + ' That\'s the one!');
+alert('Congrats ' + (userName) + ',' + ' That\'s the one!');
 
 
 alert(" You got "  + questionRight + " out of 5 questions correct!");
